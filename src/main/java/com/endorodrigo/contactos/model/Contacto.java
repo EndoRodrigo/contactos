dbde040d29831f9298b5b8c3eec056841e935ed9
Endo Rodrigo Rodriguez Ariza <endorrodriguez@ucompensar.edu.co>
2025-05-24 16:59:28 -0500

    feat: creacion del repositorio contactorepositorio

--- src/main/java/com/endorodrigo/contactos/model/Contacto.java
+++ src/main/java/com/endorodrigo/contactos/model/Contacto.java
@@ -19,7 +19,7 @@ public class Contacto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String nombre;
     private String celular;
     private String email;
